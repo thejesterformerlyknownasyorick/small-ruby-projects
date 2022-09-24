0d8a1538b5fd1bb3e0a51b42ab6275f04875b92b
thejesterformerlyknownasyorick <42towels@gmail.com>
2022-09-23 22:34:07 -0700

    fix while statements to not cause endless loop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,14 +4,16 @@ def bubble_sort(array)
         x = 0
         while i < array.length
             y = i + 1
-            while y =< array.length
-                if array[i] > array.[y]
+            while y < array.length
+                if array[i] > array[y]
                     holder = array[i]
                     array[i] = array[y]
                     array[y] = holder
                     x += 1
                 end
+                y += 1
             end
+            i += 1
         end
         break if x == 0
     end
@@ -19,4 +21,4 @@ array
 end
     
 
-p bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4,3,78,2,0,2,0,0,2,1,657,3,2,7456,3,1,0])
