39ab373ed5d23ee7de2607daf5915b1448ecc02b
thejesterformerlyknownasyorick <42towels@gmail.com>
2022-09-23 20:36:25 -0700

    Accept multiple words in string and check each word individually, then return each word found

--- substrings.rb
+++ substrings.rb
@@ -1,8 +1,12 @@
 dictionary_array = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, dictionary)
-    p dictionary.select { |word| word == string.downcase }
+    word_array = string.split(" ")
+    i = 0
+    while i < word_array.length
+        p dictionary.select { |word| word == word_array[i].downcase }
+        i += 1
+    end
 end
 
-substrings("below", dictionary_array)
-p dictionary_array
+substrings("below them Below Go GO gO going go down how howdy Horn", dictionary_array)
