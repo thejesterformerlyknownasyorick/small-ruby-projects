0753990fc0d95ed1edaa25fb4c554dc67917a1ea
thejesterformerlyknownasyorick <42towels@gmail.com>
2022-10-09 12:17:31 -0700

    Add insert and delete functionality

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -30,10 +30,66 @@ class Tree
         root
     end
 
+    def insert (value, node = root)
+        if value == node.data
+            return nil
+        elsif value < node.data
+            node.left.nil? ? node.left = Node.new(value) : insert(value, node.left)
+        else
+            node.right.nil? ? node.right = Node.new(value) : insert(value, node.right)
+        end
+    end
+
+    def delete(value, node = root)
+        if node == nil
+            return nil
+        elsif value < node.data
+            if node.left.data == value
+                if node.left.left.nil? && node.left.right.nil?
+                    node.left.data = nil
+                    node.left = nil
+                elsif node.left.left.nil?
+                    node.left.data = nil
+                    node.left = node.left.right
+                else
+                    node.right.data = min(node.right)
+                    delete(node.right.data, node.right.right)
+                end
+            else
+                delete(value, node.left)
+            end
+        elsif value > node.data
+            if node.right.data == value
+                if node.left.left.nil?  && node.left.right.nil?
+                    node.right.data = nil
+                    node.left = nil
+                elsif node.right.right.nil?
+                    node.right.data = nil
+                    node.right = node.right.left
+                else
+                    node.right.data = min(node.right)
+                end
+            else
+                delete(value, node.right)
+            end
+        end
+    end
+
     def pretty_print(node = @root, prefix = '', is_left = true)
         pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
         puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
         pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    end
+
+    def min(node)
+        
+        current_node = node
+
+        until current_node.left == nil
+            current_node = current_node.right.left
+        end
+
+        current_node.data
     end
 
 end
@@ -41,5 +97,12 @@ end
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 tree = Tree.new(array)
+
+tree.pretty_print
+
+tree.insert(65)
+tree.insert(66)
+tree.insert(64)
+tree.delete(23)
 
 tree.pretty_print
